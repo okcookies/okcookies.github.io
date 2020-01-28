7182bbed45fdcdbcef88b25a905f99bb22f6956d
traviscibot <travis@travis-ci.org>
2020-01-28 08:33:23 +0000

    Sync repo [skip ci]

--- okcookies.js
+++ okcookies.js
@@ -1,77 +1,115 @@
-let currentScript = document.currentScript;
+"use strict";
 
-let cookieName = null;
+class OkCookies {
+    constructor(okCookiesSettings=new Map([[]])) {
+        let currentScript = document.currentScript;
+
+        let cookieName = (function() {
+            if (okCookiesSettings.has("cookieName") === false) {
+                try {
+                    return currentScript.attributes.cookieName.nodeValue;
+                }
+                catch (TypeError) {
+                    return window.location.hostname + "-okcookies";
+                }
+            }
+            else {
+                return okCookiesSettings.get("cookieName")
+            }
+        })();
+
+        let customOkCookiesMessage = (function() {
+            if (okCookiesSettings.has("customOkCookiesMessage") === false) {
+                try {
+                    return currentScript.attributes.message.nodeValue;
+                }
+                catch (TypeError) {
+                    return `
+                <p>This site uses cookies to enhance your browsing experience. By
+                    using this site you agree to the use of cookies and you acknowledge that you have
+                    read and understood our terms and conditions and, privacy policy.
+                    <button onclick="closeAndAccept()">Close and Accept</button>
+                </p>`;
+                }
+            }
+            else {
+                return okCookiesSettings.get("customOkCookiesMessage");
+            }
+        })();
+
+        let customStyle = (function() {
+            if (okCookiesSettings.has("customStyle") === false) {
+                try {
+                    return Boolean(currentScript.attributes.customStyle.nodeValue);
+                }
+                catch (TypeError) {
+                    return false;
+                }
+            }
+            else {
+                return okCookiesSettings.get("customStyle");
+            }
+        })();
+
+        this.css = `
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #e6e6e6;
+    opacity: 0.85;
+    text-align: center;
+    `;
+
+        this.setCookie = function() {
+            let date = new Date();
+            date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+            let expires = "expires=" + date.toGMTString();
+            let cookieValue = "true";
+            document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+        }
+
+        let getCookie = function () {
+            let name = cookieName + "=true";
+            let decodedCookie = decodeURIComponent(document.cookie);
+            let splittedCookies = decodedCookie.split(';');
+            if (splittedCookies.includes(name) || splittedCookies.includes(" " + name)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        this.checkCookie = function () {
+            if (!getCookie()) {
+                // show okcookies code
+                let getBody = document.getElementsByTagName('body')[0];
+                let appendString = `<div id="okcookies">` + customOkCookiesMessage + `</div>`;
+                getBody.innerHTML += appendString;
+                if (customStyle === false) {
+                    let okcookiesDiv = document.getElementById("okcookies");
+                    okcookiesDiv.setAttribute("style", this.css);
+                }
+            }
+        };
+    }
+}
+;
+
+let okCookies;
+
 try {
-    cookieName = currentScript.attributes.cookieName.nodeValue;
-} catch(TypeError) {
-    cookieName = window.location.hostname + "-okcookies";
+    okCookiesSettings;
+    okCookies = new OkCookies(okCookiesSettings);
+} catch (ReferenceError) {
+    okCookies = new OkCookies();
 }
 
-let message = null;
-try {
-    message = currentScript.attributes.message.nodeValue;
-} catch(TypeError) {
-    message = `
-    <p>This site uses cookies to enhance your browsing experience. By
-        using this site you agree to the use of cookies and you acknowledge that you have
-        read and understood our terms and conditions and, privacy policy.</a>
-        <button onclick="closeAndAccept()">Close and Accept</button>
-    </p>`;
-}
-
-let customStyle = null;
-try {
-    customStyle = Boolean(currentScript.attributes.customStyle.nodeValue);
-} catch(TypeError) {
-    customStyle = false;
-}
-
-const css = `
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: #e6e6e6;
-opacity: 0.85;
-text-align: center;
-`
-
-checkCookie();
-
-function closeAndAccept() {
+function closeAndAccept () {
     let gdprDiv = document.getElementById("okcookies");
     gdprDiv.style.display = "none";
-    setCookie();
+    okCookies.setCookie();
 }
 
-function setCookie() {
-    let date = new Date();
-    date.setTime(date.getTime() + (365*24*60*60*1000));
-    let expires = "expires=" + date.toGMTString();
-    cookieValue = "true";
-    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
-}
-
-function getCookie() {
-    let name = cookieName + "=true";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let splittedCookies = decodedCookie.split(';');
-    if (splittedCookies.includes(name) || splittedCookies.includes(" " + name)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function checkCookie() {
-    if (!getCookie()) {
-        // show okcookies code
-        let getBody = document.getElementsByTagName('body')[0];
-        let appendString = `<div id="okcookies">` + message + `</div>`;
-        getBody.innerHTML += appendString;
-        if (customStyle === false) {
-            let okcookiesDiv = document.getElementById("okcookies");
-            okcookiesDiv.setAttribute("style", css);
-        }
-    }
-}
+okCookies.checkCookie();
